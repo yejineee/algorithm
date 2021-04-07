38f7b1b68df7aa2717af9979e46b1bc871220dd2
yejineee <yygeniee@gmail.com>
2021-04-07 16:47:27 +0900

    Add version 1 of determine-if-string-halves-are-alike 21.04.07

---
+++ 2021-04/v1-determine-if-string-halves-are-alike.js
@@ -0,0 +1,14 @@
+const countOfVowels = (str) => {
+  const vowels = ["a", "e", "i", "o", "u"];
+  const count = Array.from(str).reduce(
+    (sum, alpha) => sum + (vowels.includes(alpha.toLowerCase()) ? 1 : 0),
+    0
+  );
+  return count;
+};
+
+const halvesAreAlike = function (s) {
+  const a = s.substring(0, s.length / 2);
+  const b = s.substring(s.length / 2);
+  return countOfVowels(a) === countOfVowels(b);
+};
