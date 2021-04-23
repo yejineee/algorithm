c8ea18b3ac9811ebd28c7d212306c3acff1a4a52
yejineee <yygeniee@gmail.com>
2021-04-23 16:48:07 +0900

    add: Count Binary Substrings - 21/04/23

---
+++ 2021-04/count-binary-strings.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var countBinarySubstrings = function (s) {
+  const groupRegex = /1+|0+/g;
+  const lenOfGroup = s.match(groupRegex).map((x) => x.length);
+  const totalCount = lenOfGroup.reduce((count, len, i) => {
+    if (i >= lenOfGroup.length - 1) {
+      return count;
+    }
+    return count + Math.min(len, lenOfGroup[i + 1]);
+  }, 0);
+  return totalCount;
+};
