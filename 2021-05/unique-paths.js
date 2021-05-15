32b416d68c7843cd80d4b473db4c1e787d74da7b
yejineee <yygeniee@gmail.com>
2021-05-15 20:39:51 +0900

    Add: 65.unique paths 2021/05/15

---
+++ 2021-05/unique-paths.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} m
+ * @param {number} n
+ * @return {number}
+ */
+const uniquePaths = function (m, n) {
+  let top = new Array(n).fill(1);
+  for (let r = 1; r < m; r += 1) {
+    const cur = [];
+    cur[0] = 1;
+    for (let c = 1; c < n; c += 1) {
+      cur[c] = cur[c - 1] + top[c];
+    }
+    top = cur;
+  }
+  return top[n - 1];
+};
+
+uniquePaths(3, 2);
