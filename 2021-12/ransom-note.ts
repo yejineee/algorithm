45e21d6acf693d5b378e83a029c9265ce7b30d30
yejineee <yygeniee@gmail.com>
2021-12-04 15:08:22 +0900

    Add: ransom note - 2021/12/04

---
+++ 2021-12/ransom-note.ts
@@ -0,0 +1,70 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString: string = '';
+let inputLines: string[] = [];
+let currentLine: number = 0;
+
+process.stdin.on('data', function(inputStdin: string): void {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function(): void {
+    inputLines = inputString.split('\n');
+    inputString = '';
+
+    main();
+});
+
+function readLine(): string {
+    return inputLines[currentLine++];
+}
+
+/*
+ * Complete the 'checkMagazine' function below.
+ *
+ * The function accepts following parameters:
+ *  1. STRING_ARRAY magazine
+ *  2. STRING_ARRAY note
+ */
+
+function checkMagazine(magazine: string[], note: string[]): void {
+    // Write your code here
+  const wordCountMap = magazine.reduce((prevWordCountMap, word) => {
+    const count = prevWordCountMap.has(word) ? prevWordCountMap.get(word) + 1 : 1;
+    prevWordCountMap.set(word, count);
+    return prevWordCountMap;
+  }, new Map());
+
+  const isInvalid = note.some((word) => {
+    if(!wordCountMap.has(word)){
+      return true;
+    }
+    const decrementedCount = wordCountMap.get(word) - 1;
+    if(decrementedCount <= 0){
+      wordCountMap.delete(word);
+    }else{
+      wordCountMap.set(word, decrementedCount);
+    }
+    return false;
+  });
+
+  const result = isInvalid ? 'No' : 'Yes';
+  console.log(result);
+}
+
+function main() {
+    const firstMultipleInput: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+    const m: number = parseInt(firstMultipleInput[0], 10);
+
+    const n: number = parseInt(firstMultipleInput[1], 10);
+
+    const magazine: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+    const note: string[] = readLine().replace(/\s+$/g, '').split(' ');
+
+    checkMagazine(magazine, note);
+}
